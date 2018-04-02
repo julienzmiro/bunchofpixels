664e90e4774db2da7e922f77df0a67d1e243cd83
Julien Zmiro <julienzmiro@Juliens-MacBook-Pro-2.local>
2018-04-02 17:53:32 +0100

    Removing uniqid

--- auth.js
+++ auth.js
@@ -2,13 +2,12 @@ const env = process.env.NODE_ENV || 'development';
 const passport = require('passport');
 const FigmaStrategy = require('passport-figma').Strategy;
 const config = require('./config')[env];
-const uniqid = require('uniqid');
 
 passport.serializeUser((user, done) => {
   done(null, user);
 });
 passport.deserializeUser((user, done) => {
-    done(null, user);
+  done(null, user);
 });
 
 passport.use(
@@ -19,7 +18,6 @@ passport.use(
     state: true
   }, (accessToken, refreshToken, profile, done) => {
     done(null, {
-      "id": uniqid(),
       "accessToken": accessToken,
       "refreshToken": refreshToken
     });
