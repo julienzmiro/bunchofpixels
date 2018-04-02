16a3d44f6310ec9779fa392ca7aad9ded35f4797
Julien Zmiro <julienzmiro@Juliens-MacBook-Pro-2.local>
2018-04-02 17:58:14 +0100

    Adding the redirect

--- routes/app.js
+++ routes/app.js
@@ -6,7 +6,11 @@ var user_controller = require('../controllers/userController');
 var team_controller = require('../controllers/teamController');
 
 router.get('/', function(req, res, next) {
-  res.render('app', { title: 'Bunch of Pixels', at: req.session.at, rt: req.session.rt});
+  if (req.session.at && req.session.rt) {
+    res.render('app', { title: 'Bunch of Pixels', at: req.session.at, rt: req.session.rt});
+  } else {
+    res.redirect('/sign-in');
+  }
 });
 
 module.exports = router;
